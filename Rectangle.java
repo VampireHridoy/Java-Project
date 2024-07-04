cc04d8f3f68cecfb5f2f8c068a1611edeffe5220
Km Rokonuzzaman <105871564+VampireHridoy@users.noreply.github.com>
2024-07-04 22:50:56 +0600

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,27 @@
+             /*Write a programe that calculate area of Rectangle*/
+                
+             // CODE BY KM ROKONUZZAMAN
+import java.util.Scanner;
+ class Rectangle
+{
+   public static Void main (String [] args) 
+   {
+      int length,width;
+    System.out.println("write row the Inputs: ");
+    Scanner sc=new Scanner(System.in); //KM ROKONUZZAMAN
+    System.out.println("Enter the length of Rectangle:");
+      length=sc.nextInt();
+    System.out.println("Enter the width of Rectangle: ");
+      width=sc.nextInt();
+    System.out.println("What is the area of Rectangle: ");
+      Area=length*width;
+     System.out.println("Area is,", area")
+
+  }
+
+   }
+
+
+
+
+
